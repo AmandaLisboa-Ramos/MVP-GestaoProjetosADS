bd001564fb5ce4e750cd3e401d4655cfd9f88020
Jhonathan Pegoral <96213633+Pegoral123@users.noreply.github.com>
2026-07-13 19:26:57 -0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -183,7 +183,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
